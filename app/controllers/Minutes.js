034a3f1648a75a492518f8e1a47da59e7b4e87f6
tiagoafseixas <tiagoafseixas@gmail.com>
2017-03-02 00:55:05 +0000

    Fixed server actions. Created delete actions.

--- app/controllers/Minutes.js
+++ app/controllers/Minutes.js
@@ -1,78 +1,57 @@
 "use strict";
 
 var Immutable = require('immutable');
+var _ = require('lodash');
 
 var mongoose = require('mongoose');
-mongoose.connect('localhost:27017');
 mongoose.Promise = global.Promise;
+mongoose.connect('localhost:27017');
 
 var Minute = require('../models/Minutes');
 
-exports.post = (req, res, next) => {
+/**
+ * exports.post - inserts a new meeting minute in the database.
+ * 
+ * @todo: rever a questão dos compacts e concats. podia e devia estar em algum
+ *        tipo de middleware.
+ */
+exports.post = (map = Immutable.Map({}), callback) => {
     console.log(">controller.minutes -> post");
 
-    let body = Immutable.Map(req.body);
     let minute = new Minute();
-    minute.title = body.get("title");
-    minute.date = new Date(body.get("date"));
-    minute.minute = body.get("minute");
-    minute.conclusions = body.get("conclusions");
+    minute.title = map.get("title");
+    minute.date = new Date(map.get("date"));
+    minute.minute = map.get("minute");
+    minute.conclusions = map.get("conclusions");
     
-    let timeInSeconds = 0;
-    let timeArray = body.get("time").split(':');
+    let timeArray = map.get("time").split(':');
+    minute.time = timeArray[0]*3600 + timeArray[1]*60;
 
-    if(timeArray.length > 1) {
-        timeInSeconds = timeArray[0]*3600 + timeArray[1]*60;
-    }
+    let agendaDescription = _.compact(_.concat([], map.get("agendaDescription")));
+    let agendaCompleted = _.compact(_.concat([], map.get("agendaItemCompleted")));
+    minute.agenda = agendaDescription.map(
+        (v, i) => { return { description : v, complete : agendaCompleted[i] } }
+    );
 
-    minute.time = timeInSeconds;
+    let invitedName = _.compact(_.concat([], map.get("invitedName")));
+    let invitedAttended = _.compact(_.concat([], map.get("invitedItemAttended")));
+    minute.invited = invitedName.map(
+        (v, i) => { return { name : v, attended : invitedAttended[i] } }
+    );
 
-    if(body.get("agendaDescription").map) {
-        minute.agenda = body.get("agendaDescription").map(
-            (value, index) => {
-                return {
-                    description : value,
-                    complete : body.get("agendaItemCompleted")[index]
-                }
-            }
-        );
-    }
+    let todoDescription = _.compact(_.concat([], map.get("todoDescription")));
+    let todoCompleted = _.compact(_.concat([], map.get("todoItemCompleted")));
+    minute.todos = todoDescription.map(
+        (v, i) => { return { description : v, complete : todoCompleted[i] } }
+    );
 
-    if (body.get("invitedName").map) {
-        minute.invited = body.get("invitedName").map(
-            (value, index) => {
-                return {
-                    name : value,
-                    attended : body.get("invitedItemAttended")[index]
-                }
-            }
-        );
-    }
-
-    if (body.get("todoDescription").map) {
-        minute.todos = body.get("todoDescription").map(
-            (value, index) => {
-                return {
-                    description : value,
-                    complete : body.get("todoItemCompleted")[index]
-                }
-            }
-        );
-    }
-
-    minute.save((err, id) => {
-        if(err) {
-            console.log(err);
-        }
-        return id;
-    });
+    minute.save((err) => { (err) ? callback({err}) : callback(null, {id: minute._id}) });
 
     console.log("<controller.minutes -> post");
 };
 
-exports.get = (req, res, next) => {
+exports.get = () => {
     console.log(">controller.minutes -> get");
-    
     return Minute
         .find()
         .limit(20)
@@ -83,4 +62,13 @@ exports.get = (req, res, next) => {
             return minutes;
         } );
     console.log("<controller.minutes -> get");
+};
+
+exports.delete = (id, callback) => {
+    return Minute.findByIdAndRemove(
+        id,
+        (err, minute) => { 
+            (err) ? callback(err) : callback(err, (minute) ? minute._id : '')
+        }
+    );
 };
--- server.js
+++ server.js
@@ -2,7 +2,12 @@ var express = require('express')
 var path = require('path')
 var compression = require('compression')
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+var Immutable = require('immutable');
+var _ = require('lodash');
 
+// INITIAL SERVER CONFIGURATION
+// =============================================================================
 var app = express();
 
 var MinutesController = require('./app/controllers/Minutes');
@@ -12,6 +17,13 @@ var MinutesController = require('./app/controllers/Minutes');
 app.use(compression());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(expressValidator({
+ customSanitizers: {
+    sanitizeMultiValue: function(value) {
+        return _.compact(_.concat([], value)) || [];
+    },
+ }
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // ROUTES FOR OUR API
@@ -36,20 +48,46 @@ router.route('/minutes')
     // Create a new minutes
     .post( (req, res, next) => {
         console.log('#server.js -> creating a new minute!');
-        console.log(req.body);
-        res.json({ id: MinutesController.post(req, res, next) });
+
+        req.checkBody('title', 'Title is required.').notEmpty();
+        req.checkBody('date', 'Not a valid date format.').notEmpty()
+            .withMessage('Date is Required.').isDate();
+        req.checkBody('time', 'Invalid time format. Hour must be HH:MM.').notEmpty()
+            .withMessage('Time is required')
+            .matches(/^[\d]{2}:[\d]{2}$/);
+        
+        let errors = req.validationErrors();
+
+        if (errors) {
+            res.status(500).json({errors});
+        } else {
+            MinutesController.post(
+                Immutable.Map(req.body),
+                (err, result) => { (err) ? res.status(500).json({err}) : res.json(result) }
+            );
+        }
     })
-    
+
     .get( (req, res, next) => {
         console.log('#server.js -> getting minutes!');
-        MinutesController.get(req, res, next).then(
-            (items) => {
-                //console.log(err);
-                console.log(items);
-                res.send({items});
-            }
-        );
-    } );
+        MinutesController.get().then( (items) => { res.send({items}); });
+    } )
+
+    .delete( (req, res, next) => {
+        console.log('#server.js -> deleting a minute!');
+        req.checkBody('id', 'A valid id to delete is required.').notEmpty();
+        let errors = req.validationErrors();
+
+        if (errors) {
+            res.status(500).json({errors});
+        } else {
+            MinutesController.delete(
+                req.body.id,
+                (err, result) => { (err) ? res.status(500).json({err}) : res.json(result) }
+            );
+        }
+    } )
+    ;
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
