6f88f30f554b0e12c9d94c24f3657e5aa3397bc0
tiagoafseixas <tiagoafseixas@gmail.com>
2017-03-02 01:08:36 +0000

    Installed Jest

---
+++ tests/minutes.tests.js
@@ -0,0 +1,11 @@
+var MinutesController = require('../app/controllers/Minutes');
+var Immutable = require('immutable');
+
+test(
+    'Test Add Minute With Error!',
+    () => {
+        let mockMinute = Immutable.Map({
+
+        });
+    }
+);
